a533ff0b437db96e9a7a89e37c3d36526e5a8240
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-05-31 21:37:22 +0100

    feat: updated the app url to use framework routers

--- core_payments/urls.py
+++ core_payments/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, VendorProfileViewSet, PaymentMethodViewSet, PayoutMethodViewSet, TransactionViewSet, PayoutViewSet
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet)
+router.register(r'vendor-profiles', VendorProfileViewSet)
+router.register(r'payment-methods', PaymentMethodViewSet)
+router.register(r'payout-methods', PayoutMethodViewSet)
+router.register(r'transactions', TransactionViewSet)
+router.register(r'payouts', PayoutViewSet)
 
 urlpatterns = [
-    path('', include())
+    path('', include(router.urls)),
 ]
